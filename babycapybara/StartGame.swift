8ac4592824c5dea2f5350d73d32d5791b752971e
Jannnn444 <yucianhuang@gmail.com>
2024-10-27 22:30:36 +0800

    Add buttons for changing character

--- babycapybara/StartGame.swift
+++ babycapybara/StartGame.swift
@@ -8,16 +8,44 @@
 import SwiftUI
 
 struct StartGame: View {
+    
+    var greetings: String = "Welcome"
+    
     var body: some View {
        
-    
-            Text("Welcome")
+        VStack{
+            Image("1")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .padding()
+            
+            Text("\(greetings)")
+                .padding()
                 .padding()
                 .font(.title)
                 .fontDesign(.monospaced)
                 .background(Color.yellow)
                 .cornerRadius(15)
-
+            
+            HStack{
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    
+                })
+                Image(systemName: "arrowtriangle.left.fill")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(.yellow)
+                    .cornerRadius(5)
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    
+                })
+                Image(systemName: "arrowtriangle.right.fill")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(.yellow)
+                    .cornerRadius(5)
+            }.padding()
+        }
     }
     
 }
